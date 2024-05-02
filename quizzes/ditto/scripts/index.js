ca7529343c6b5b61db87943a0378098c4023a99d
Allison <53547310+ajbanayat@users.noreply.github.com>
2024-05-02 17:56:40 -0500

    Added template for quiz

--- quizzes/ditto/scripts/index.js
+++ quizzes/ditto/scripts/index.js
@@ -4,6 +4,21 @@ function startQuiz() {
     const startPageDiv = document.getElementById("startPage");
     const quizPageDiv = document.getElementById("quizPage");
 
-    startPageDiv.setAttribute("hidden", "true");
-    quizPageDiv.removeAttribute("hidden");
+    startPageDiv.classList.add("hidden");
+    quizPageDiv.classList.remove("hidden");
+
+    nextQuestion();
 }
+
+var questionNum = 0;
+function nextQuestion() {
+    const startPageDiv = document.getElementById("startPage");
+    const quizPageDiv = document.getElementById("quizPage");
+    questionNum++;
+}
+
+var questions = [
+    { question: "Question 1", answers: [ "Answer 1", "Answer 2" ] },
+    { question: "Question 2", answers: [ "Answer 1", "Answer 2" ] },
+    { question: "Question 3", answers: [ "Answer 1", "Answer 2" ] },
+]
